aab418835d991b74622803104b43ac85e8268621
Wesley Carr <surfswell17@gmail.com>
2013-08-19 09:34:28 -0400

    Init - gemfile. Not yet seeded firefighters / fires

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FireHouse::Application.config.secret_token = '40ded6b1af4d314fc6d5b7818da5437d220db57d65585237afb026d7bef4aacea8375c24bd6f0a2ab56d30100fe4459ab3129336e503794ed81f11f7c30d8e16'
